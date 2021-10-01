cd71603fc36f408e419afad27c508aa33423a659
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-01 14:36:09 -0400

    Update Main.java

--- Dictionary_word/src/Dictionary_word/Main.java
+++ Dictionary_word/src/Dictionary_word/Main.java
@@ -1,7 +1,17 @@
 package Dictionary_word;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
-
+        Search_Words SW = new Search_Words();
+        String user_string = "LOVEISPATIENT";
+        boolean bool;
+        List<String> dictionary_list = new ArrayList<String>();
+           dictionary_list = Arrays.asList("LOVE","IS","PATIENT");
+        bool = SW.Search_Words(user_string, dictionary_list);
+        System.out.println(bool);
     }
 }
