22e6b71d49dda4731f4cfc684d6867618ae951ac
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-01 14:38:16 -0400

    Update Main.java

--- Dictionary_word/src/Dictionary_word/Main.java
+++ Dictionary_word/src/Dictionary_word/Main.java
@@ -4,14 +4,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/*This class is the main class.
+The user input string can be changed in the hardcoded line. The dictionary list also can ba inserted by the user.
+After compiling the program, the output will be seen whether the user string can be broken into each words reserved in the dictionary list. */ 
+
 public class Main {
     public static void main(String[] args){
-        Search_Words SW = new Search_Words();
-        String user_string = "LOVEISPATIENT";
+        Search_Words SW = new Search_Words(); 
+        String user_string = "LOVEISPATIENT"; //The user can change the user input string here.
         boolean bool;
-        List<String> dictionary_list = new ArrayList<String>();
-           dictionary_list = Arrays.asList("LOVE","IS","PATIENT");
+        List<String> dictionary_list = new ArrayList<String>(); //Initialize the list of dictionary.
+           dictionary_list = Arrays.asList("LOVE","IS","PATIENT"); //The user can change the dictionary list here.
         bool = SW.Search_Words(user_string, dictionary_list);
         System.out.println(bool);
     }
-}
+}//Main
